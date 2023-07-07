5dce42d3b715b3b95c3abba1608178b521570990
gr664 <greschik.gergely@gmail.com>
2023-07-07 18:42:38 +0200

    trying new about page script

--- omsz_calc/src/App.js
+++ omsz_calc/src/App.js
@@ -1,34 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Ez egy NEM HIVATALOS webApp, ami az OMSZ kivonuló állományának készítettem.
-//           A dolgozott műszakok megadása után a program kiszámolja a bruttó fizetésed nagyságát.
-//           Ha hibát észlelsz, szólj nekem!
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Kiszámolom a fizetésem
-//         </a>
-//       </header>
-//       <footer>
-//       Icon by <a href="https://freeicons.io/profile/350417">Barudak Lier</a> on <a href="https://freeicons.io">freeicons.io</a>
-//       </footer>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
--- omsz_calc/src/pages/about.js
+++ omsz_calc/src/pages/about.js
@@ -1,14 +1,30 @@
-import React from "react";
+import logo from './logo.svg';
+import './App.css';
 
-const About = () => {
-	return (
-		<div>
-			<h1>
-				GeeksforGeeks is a Computer
-				Science portal for geeks.
-			</h1>
-		</div>
-	);
-};
-
-export default About;
+function About() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Ez egy NEM HIVATALOS webApp, ami az OMSZ kivonuló állományának készítettem.
+            A dolgozott műszakok megadása után a program kiszámolja a bruttó fizetésed nagyságát.
+            Ha hibát észlelsz, szólj nekem!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Kiszámolom a fizetésem
+          </a>
+        </header>
+        <footer>
+        Icon by <a href="https://freeicons.io/profile/350417">Barudak Lier</a> on <a href="https://freeicons.io">freeicons.io</a>
+        </footer>
+      </div>
+    );
+  }
+  
+  export default About;
