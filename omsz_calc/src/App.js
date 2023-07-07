5fd420a245daa126428206c8e759b1cc1abe293f
gr664 <greschik.gergely@gmail.com>
2023-07-07 15:52:23 +0200

    updated app.js

--- omsz_calc/src/App.js
+++ omsz_calc/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Kiszámolom a fizetésem
         </a>
       </header>
     </div>
