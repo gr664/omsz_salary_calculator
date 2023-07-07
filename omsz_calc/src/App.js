c8f3f546ba0cf483e9c7ad351345e25032d1da1e
gr664 <greschik.gergely@gmail.com>
2023-07-07 15:35:18 +0200

    updated hungarian characers

--- omsz_calc/src/App.js
+++ omsz_calc/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Ez a honlap az OMSZ kivonuló állományának készült, hogy kiszámolhassák a fizetésüket.
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
